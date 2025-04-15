8fc54601cce48ccec9897003863983d0713ce0a2
Johnny Tahirov <jeyhunt@gmail.com>
2025-04-14 21:34:11 -0700

    add terminal colors

--- init.lua
+++ init.lua
@@ -8,5 +8,23 @@ else
 	if vim.g.neovide then
 		vim.g.neovide_opacity = 1
 		vim.g.neovide_normal_opacity = 1
+
+		-- terminal colors took from tokyo-night theme
+		vim.g.terminal_color_0 = "#a1a1a1"
+		vim.g.terminal_color_1 = "#f7768e"
+		vim.g.terminal_color_2 = "#73daca"
+		vim.g.terminal_color_3 = "#e0af68"
+		vim.g.terminal_color_4 = "#7aa2f7"
+		vim.g.terminal_color_5 = "#bb9af7"
+		vim.g.terminal_color_6 = "#7dcfff"
+		vim.g.terminal_color_7 = "#c0caf5"
+		vim.g.terminal_color_8 = "#adadad"
+		vim.g.terminal_color_9 = "#f7768e"
+		vim.g.terminal_color_10 = "#73daca"
+		vim.g.terminal_color_11 = "#e0af68"
+		vim.g.terminal_color_12 = "#7aa2f7"
+		vim.g.terminal_color_13 = "#bb9af7"
+		vim.g.terminal_color_14 = "#7DCFFF"
+		vim.g.terminal_color_15 = "#C0CaF5"
 	end
 end
