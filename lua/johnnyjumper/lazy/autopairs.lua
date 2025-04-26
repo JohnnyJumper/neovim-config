9db451fd8682c27b3965f9e72d32f76e7ef25792
Johnny Tahirov <jeyhunt@gmail.com>
2025-04-25 17:33:29 -0700

    migrate to mini-pairs

--- lua/johnnyjumper/lazy/autopairs.lua
+++ lua/johnnyjumper/lazy/autopairs.lua
@@ -1,8 +1,16 @@
 return {
-	"altermo/ultimate-autopair.nvim",
-	event = { "InsertEnter", "CmdlineEnter" },
-	branch = "v0.6", --recommended as each new version will have breaking changes
+	"echasnovski/mini.pairs",
+	event = "VeryLazy",
 	opts = {
-		--Config goes here
+		mappings = {
+			-- disable if a matching character is in an adjacent position (eg. fixes
+			-- markdown triple ticks) neigh_pattern: a pattern for *two* neighboring
+			-- characters (before and after). Use dot `.` to allow any character.
+			-- Here, we disable the functionality instead of inserting a matching quote
+			-- if there is an adjacent non-space character
+			["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+			['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+			["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+		},
 	},
 }
