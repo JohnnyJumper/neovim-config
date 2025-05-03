11524b0251a725240a56124a1b4c2da338b18fc6
Johnny Tahirov <jeyhunt@gmail.com>
2025-05-03 13:35:26 -0700

    update alpha to point into my config directly

--- lua/johnnyjumper/lazy/alpha.lua
+++ lua/johnnyjumper/lazy/alpha.lua
@@ -30,7 +30,11 @@ return {
 			dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
 			dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
 			dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-			dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | pwd<CR>"),
+			dashboard.button(
+				"s",
+				"  > Settings",
+				":e $HOME/.config/nvim/lua/johnnyjumper/init.lua | :cd %:p:h | pwd<CR>"
+			),
 			dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 			{
 				type = "text",
