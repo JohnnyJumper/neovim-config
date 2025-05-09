27f5ffc54457c3b9d1ade641d89c497feabe2af4
Johnny Tahirov <jeyhunt@gmail.com>
2025-05-08 19:51:33 -0700

    configure lualine

--- lua/johnnyjumper/lazy/lualine.lua
+++ lua/johnnyjumper/lazy/lualine.lua
@@ -5,32 +5,35 @@ return {
 		"folke/noice.nvim",
 	},
 	config = function()
+		local function getFilePath()
+			return vim.fn.expand("%:p")
+		end
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
 				theme = "auto",
+				component_separators = "",
 			},
 			sections = {
+				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+				lualine_b = {
+					"branch",
+					"diff",
+					"diagnostics",
+				},
+				lualine_c = {
+					"%=",
+					{ getFilePath },
+				},
 				lualine_x = {
-					{
-						require("noice").api.status.message.get_hl,
-						cond = require("noice").api.status.message.has,
-					},
 					{
 						require("noice").api.status.command.get,
 						cond = require("noice").api.status.command.has,
-						color = { fg = "#ff9e64" },
 					},
-					{
-						require("noice").api.status.mode.get,
-						cond = require("noice").api.status.mode.has,
-						color = { fg = "#ff9e64" },
-					},
-					{
-						require("noice").api.status.search.get,
-						cond = require("noice").api.status.search.has,
-						color = { fg = "#ff9e64" },
-					},
+				},
+				lualine_z = {
+					{ "%p%%/%L", separator = { right = "" }, left_padding = 2 },
 				},
 			},
 		})
