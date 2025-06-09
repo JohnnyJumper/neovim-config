f3da8006955e399137ec2afa63e835a44c976a22
Johnny Tahirov <jeyhunt@gmail.com>
2025-06-08 21:38:27 -0700

    try to fix lsp
    also added a way to start the tsgo lsp as a trial

--- lua/johnnyjumper/lazy/lsp.lua
+++ lua/johnnyjumper/lazy/lsp.lua
@@ -2,7 +2,11 @@ return {
 	{
 		"pmizio/typescript-tools.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
+		opts = {
+			settings = {
+				tsserver_path = "/home/zurzula/.local/share/pnpm/global/5/.pnpm/typescript@5.7.3/node_modules/typescript/bin/tsserver",
+			},
+		},
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -15,6 +19,72 @@ return {
 
 		config = function()
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+			-- 	cmd = { vim.loop.os_homedir() .. "/typescript-go/built/local/tsgo", "--lsp", "-stdio" },
+			-- 	filetypes = {
+			-- 		"javascript",
+			-- 		"jacascriptreact",
+			-- 		"javascript.jsx",
+			-- 		"typescript",
+			-- 		"typescriptreact",
+			-- 		"typescript.tsx",
+			-- 	},
+			-- 	root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+			-- })
+
+			-- vim.lsp.enable("ts_go_ls")
+			local function custom_publish_diagnostics(_, result, ctx, config)
+				if not result.diagnostics then
+					return
+				end
+
+				-- Filter out diagnostics with the specific ESLint rule
+				result.diagnostics = vim.tbl_filter(function(diagnostic)
+					return diagnostic.code ~= "@typescript-eslint/no-unused-vars"
+				end, result.diagnostics)
+
+				-- Use the default handler to publish the filtered diagnostics
+				vim.diagnostic.handlers.default[1](_, result, ctx, config)
+			end
+
+			local server_overrides = {
+				eslint = function()
+					require("lspconfig").eslint.setup({
+						capabilities = capabilities,
+						on_attach = function(client, bufnr)
+							client.handlers["textDocument/publishDiagnostics"] = custom_publish_diagnostics
+						end,
+					})
+				end,
+				harper_ls = function()
+					require("lspconfig").harper_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							["harper_ls"] = {
+								linters = {
+									SentenceCapitalization = false,
+									ToDoHyphen = false,
+								},
+							},
+						},
+					})
+				end,
+				lua_ls = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								runtime = { version = "Lua 5.1" },
+								diagnostics = {
+									globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+								},
+							},
+						},
+					})
+				end,
+			}
+
 			require("mason").setup()
 			require("mason-lspconfig").setup({
 				automatic_installation = true,
@@ -24,39 +94,15 @@ return {
 					-- "ts_ls",
 				},
 				handlers = {
-					function(server_name) -- default handler (optional)
-						require("lspconfig")[server_name].setup({
-							capabilities = capabilities,
-						})
-					end,
-
-					["lua_ls"] = function()
+					function(server_name)
 						local lspconfig = require("lspconfig")
-						lspconfig.lua_ls.setup({
-							capabilities = capabilities,
-							settings = {
-								Lua = {
-									runtime = { version = "Lua 5.1" },
-									diagnostics = {
-										globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-									},
-								},
-							},
-						})
-					end,
-					["harper_ls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.harper_ls.setup({
-							capabilities = capabilities,
-							settings = {
-								["harper-ls"] = {
-									linters = {
-										SentenceCapitalization = false,
-										ToDoHyphen = false,
-									},
-								},
-							},
-						})
+						if server_overrides[server_name] then
+							server_overrides[server_name]()
+						else
+							lspconfig[server_name].setup({
+								capabilities = capabilities,
+							})
+						end
 					end,
 				},
 			})
@@ -67,7 +113,6 @@ return {
 				end
 				return message
 			end
-
 			vim.diagnostic.config({
 				-- update_in_insert = true,
 				float = {
@@ -91,12 +136,14 @@ return {
 				virtual_lines = {
 					current_line = true,
 					severity = { min = "ERROR" },
+					line_hl_group = "DiagnosticError",
 					format = function(diagnostic)
 						local max_length = 125
 						return "● " .. truncate_message(diagnostic.message, max_length)
 					end,
 				},
 			})
+			vim.lsp.set_log_level("OFF")
 		end,
 	},
 }
