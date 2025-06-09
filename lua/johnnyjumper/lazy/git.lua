ef4e2a7e35bbcf0a66843c009f3e09cac06aca15
Johnny Tahirov <jeyhunt@gmail.com>
2025-06-08 21:37:33 -0700

    move git blame out
    add gitlinker and git blame

---
+++ lua/johnnyjumper/lazy/git.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"linrongbin16/gitlinker.nvim",
+		cmd = "GitLink",
+		opts = {},
+		keys = {
+			{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+			{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+		},
+	},
+	{
+		"f-person/git-blame.nvim",
+		-- load the plugin at startup
+		event = "VeryLazy",
+		-- Because of the keys part, you will be lazy loading this plugin.
+		-- The plugin will only load once one of the keys is used.
+		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
+		-- or lazy = false. One of both options will work.
+		opts = {
+			enabled = true,
+			message_template = " <summary> • <date> • <author> • <<sha>>",
+			date_format = "%m-%d-%Y %S",
+			virtual_text_column = 60,
+			highlight_group = "Question",
+		},
+	},
+}
